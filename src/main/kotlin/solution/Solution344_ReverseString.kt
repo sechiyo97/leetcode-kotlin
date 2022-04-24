689977424eca9974b0af5f5de2086984b2b03841
sechiyo97 <sechiyo97@daum.net>
2022-04-25 04:25:54 +0900

    complete solution 344

--- src/main/kotlin/solution/Solution344_ReverseString.kt
+++ src/main/kotlin/solution/Solution344_ReverseString.kt
@@ -10,7 +10,14 @@ class Solution344_ReverseString: Solution<CharArray, CharArray>(){
     )
 
     override fun algorithm(params: CharArray): CharArray {
-        return params
+        var temp: Char
+        val length = params.size
+        for (i in 0 until length/2) {
+            temp = params[i]
+            params[i] = params[length-1-i]
+            params[length-1-i] = temp
+        }
+        return params // no return value in actual problem
     }
 
     override fun inputToParamType(input: String): CharArray {
