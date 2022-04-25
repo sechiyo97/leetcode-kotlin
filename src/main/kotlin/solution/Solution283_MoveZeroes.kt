08ceffbe81decede117df975ced1cec993c16c26
sechiyo97 <sechiyo97@daum.net>
2022-04-26 05:35:43 +0900

    implement Solution.InPlaceArray in MoveZeroes solution

--- src/main/kotlin/solution/Solution283_MoveZeroes.kt
+++ src/main/kotlin/solution/Solution283_MoveZeroes.kt
@@ -3,18 +3,20 @@ package solution
 import Solution
 import common.toIntArray
 
-// must use same variable for input and output
-class Solution283_MoveZeroes: Solution<IntArray, IntArray>() {
+class Solution283_MoveZeroes: Solution.InPlaceArray<IntArray, IntArray>() {
     override val givenTestCases: Map<String, IntArray> = mapOf(
         "[0,1,0,3,12]" to intArrayOf(1,3,12,0,0),
         "[0]" to intArrayOf(0)
     )
 
-    override fun algorithm(params: IntArray): IntArray {
-        return params // no return value in actual problem
+    override fun algorithm(input: IntArray) {
     }
 
-    override fun inputToParamType(input: String): IntArray {
+    override fun inputStringToInputType(input: String): IntArray {
         return input.toIntArray()
     }
+    override fun inputTypeToArrayType(input: IntArray): IntArray {
+        return input
+    }
+
 }
