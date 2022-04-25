a5f094d3cab96a8f733c4e94a9c485995c24abe0
sechiyo97 <sechiyo97@daum.net>
2022-04-26 02:24:37 +0900

    add first solutuion of problem 189

--- src/main/kotlin/solution/Solution189_RotateArray.kt
+++ src/main/kotlin/solution/Solution189_RotateArray.kt
@@ -9,11 +9,24 @@ class Solution189_RotateArray: Solution<Solution189_RotateArray.Params, IntArray
 
     override val givenTestCases: Map<String, IntArray> = mapOf(
         "[1,2,3,4,5,6,7] 3" to intArrayOf(5,6,7,1,2,3,4),
-        "[-1,-100,3,99] 2" to intArrayOf(3,99,-1,100)
+        "[-1,-100,3,99] 2" to intArrayOf(3,99,-1,-100)
     )
 
+    /**
+     * Algorithm #1: create a new IntArray and put rotated values
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
     override fun algorithm(params: Params): IntArray {
-        return params.nums // no return value in actual problem
+        val nums = params.nums
+        val k = params.k
+
+        val n = nums.size
+        val newArray = IntArray(n)
+        for (i in 0 until n) newArray[(i+k)%n] = nums[i]
+        for (i in 0 until n) nums[i] = newArray[i]
+
+        return nums // no return value in actual problem
     }
 
     override fun inputToParamType(input: String): Params {
