4239738adca0982986171a294eed785ae394f1c1
sechiyo97 <sechiyo97@daum.net>
2022-04-26 03:25:44 +0900

    add 2nd solution for problem 189

--- src/main/kotlin/solution/Solution189_RotateArray.kt
+++ src/main/kotlin/solution/Solution189_RotateArray.kt
@@ -1,6 +1,7 @@
 package solution
 
 import Solution
+import common.swap
 import common.toIntArray
 
 // must use same variable for input and output
@@ -9,7 +10,9 @@ class Solution189_RotateArray: Solution<Solution189_RotateArray.Params, IntArray
 
     override val givenTestCases: Map<String, IntArray> = mapOf(
         "[1,2,3,4,5,6,7] 3" to intArrayOf(5,6,7,1,2,3,4),
-        "[-1,-100,3,99] 2" to intArrayOf(3,99,-1,-100)
+        "[-1,-100,3,99] 2" to intArrayOf(3,99,-1,-100),
+        "[-1] 2" to intArrayOf(-1),
+        "[-1,-2] 23" to intArrayOf(-2,-1)
     )
 
     /**
@@ -17,14 +20,32 @@ class Solution189_RotateArray: Solution<Solution189_RotateArray.Params, IntArray
      * Time Complexity: O(n)
      * Space Complexity: O(n)
      */
+//    override fun algorithm(params: Params): IntArray {
+//        val nums = params.nums
+//        val k = params.k
+//
+//        val n = nums.size
+//        val newArray = IntArray(n)
+//        for (i in 0 until n) newArray[(i+k)%n] = nums[i]
+//        for (i in 0 until n) nums[i] = newArray[i]
+//
+//        return nums // no return value in actual problem
+//    }
+
+    /**
+     * Algorithm #2: use characteristics of array rotation - sub-order remains
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     override fun algorithm(params: Params): IntArray {
         val nums = params.nums
         val k = params.k
 
         val n = nums.size
-        val newArray = IntArray(n)
-        for (i in 0 until n) newArray[(i+k)%n] = nums[i]
-        for (i in 0 until n) nums[i] = newArray[i]
+        val simpleK = k%n
+        for (i in 0 until (n-simpleK)/2) nums.swap(i, (n-1)-simpleK-i)
+        for (i in 0 until simpleK/2) nums.swap((n-simpleK)+i, (n-1)-i)
+        for (i in 0 until n/2) nums.swap(i, (n-1)-i)
 
         return nums // no return value in actual problem
     }
