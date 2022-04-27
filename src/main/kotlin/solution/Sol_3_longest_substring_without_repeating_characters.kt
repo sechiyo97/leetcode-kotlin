bf031389f54a02955809745bfb09637a82f3cafc
sechiyo97 <sechiyo97@daum.net>
2022-04-28 02:38:51 +0900

    create solution 3 class

---
+++ src/main/kotlin/solution/Sol_3_longest_substring_without_repeating_characters.kt
@@ -0,0 +1,17 @@
+package solution
+
+class Sol_3_longest_substring_without_repeating_characters : Solution.General<String, Int>() {
+    override val givenTestCases = mapOf(
+        "abcabcbb" to 3,
+        "bbbbb" to 1,
+        "pwwkew" to 3
+    )
+
+    override fun algorithm(input: String): Int {
+        return 0
+    }
+
+    override fun inputStringToInputType(input: String): String {
+        return input
+    }
+}
