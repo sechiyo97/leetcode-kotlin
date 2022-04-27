24e43c4ac9c9fb21c28e2c24087d27d493b49f10
sechiyo97 <sechiyo97@daum.net>
2022-04-28 00:35:15 +0900

    change update-readme.py to record the number of problems solved, along with difficulties
    (cherry picked from commit dfa720ae85706604081e11b8998184adc6eaa292)

--- update-readme.py
+++ update-readme.py
@@ -12,57 +12,94 @@ leetcode_query_get_question_info = """
     }
 """
 
-header =""" \
-# leetcode-kotlin
-LeetCode Algorithm Practice
-            
-## Problem Solved
-\#  | name | link | difficulty
-------------- | ------------- | ------------- | -------------
+title_project = "# leetcode-kotlin"
+desc_project = "LeetCode Algorithm Practice"
+
+title_problem_solved = "## Problem Solved"
+
+table_header_total = """\
+Total |
+------------- |\
+"""
+table_header_difficulty = """\
+Easy | Medium | Hard
+------------- | ------------- | -------------\
+"""
+table_header_problems = """\
+\# | name | link | difficulty | accepted rate
+------------- | ------------- | ------------- | ------------- | -------------\
 """
 
+
+def get_table_content_string_total(problems):
+    return str(len(problems)) + " |"
+
+def get_table_content_string_difficulty(problems):
+    easy_count = sum(problem.difficulty == 'Easy' for problem in problems)
+    medium_count = sum(problem.difficulty == 'Medium' for problem in problems)
+    hard_count = sum(problem.difficulty == 'Hard' for problem in problems)
+    return str(easy_count) + " | " + str(medium_count) + " | " + str(hard_count)
+
+def get_table_content_string_problems(problems):
+    content = ""
+    for problem in problems:
+        content += problem.to_read_me_line()
+    return content
+    
+
 class Problem:
-    def __init__(self, problem_number, problem_name):
+    def __init__(self, problem_number, problem_ref_name):
         self.problem_number = problem_number
-        self.problem_name = problem_name
-        self.__updateInfo()
+        self.problem_ref_name = problem_ref_name
+        self.__update_name()
+        self.__update_info()
+
+    def __update_name(self):
+        self.problem_name = ' '.join([name[0].upper() + name[1:].lower() for name in problem_ref_name.split('-')])
         
-    def __updateInfo(self):
+    def __update_info(self):
         graphql_request = leetcode.GraphqlQuery(
             query=leetcode_query_get_question_info,
-            variables=leetcode.GraphqlQueryGetQuestionDetailVariables(title_slug=self.problem_name),
+            variables=leetcode.GraphqlQueryGetQuestionDetailVariables(title_slug=self.problem_ref_name),
             operation_name="getQuestionDetail"
         )
 
         info = leetcode_api_instance.graphql_post(body=graphql_request)
         question = info.data.question
         self.difficulty = question.difficulty
-        self.stats = question.stats
+        self.acRate = eval(question.stats)['acRate']
+
+    def get_problem_link_title(self):
+        return self.problem_number + ". " + self.problem_name
         
-    def toReadMeLine(self):
+    def get_problem_link(self):
+        return "https://leetcode.com/problems/" + self.problem_ref_name
+        
+    def to_read_me_line(self):
         return self.problem_number +\
             " | " + self.problem_name +\
-            " | " + "https://leetcode.com/problems/" + self.problem_name +\
+            " | " + "["+self.get_problem_link_title()+"]("+self.get_problem_link()+")" +\
             " | " + self.difficulty +\
+            " | " + self.acRate +\
             "\n"
-    
+
 solution_files = [f for f in listdir("src/main/kotlin/solution/") if f[0:3]=="Sol"]
 
 problems = []
 for solution_file in solution_files:
     split = solution_file.split(".")[0].split("_")
     problem_number = split[1]
-    problem_name = "-".join(split[2:])
-    problem = Problem(problem_number, problem_name)
+    problem_ref_name = "-".join(split[2:])
+    problem = Problem(problem_number, problem_ref_name)
     problems.append(problem)
     
-
 problems.sort(key=lambda solution: int(solution.problem_number))
 
 readme = open("README.md", "w")
-readme.write(header)
-
-for problem in problems:
-    readme.write(problem.toReadMeLine())
+readme.write(title_project + "\n" + desc_project + "\n\n")
+readme.write(title_problem_solved + "\n")
+readme.write(table_header_total + "\n" + get_table_content_string_total(problems) + "\n\n")
+readme.write(table_header_difficulty + "\n" + get_table_content_string_difficulty(problems) + "\n\n")
+readme.write(table_header_problems + "\n" + get_table_content_string_problems(problems))
 
 readme.close()
