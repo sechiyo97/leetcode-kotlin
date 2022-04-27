bc6afa9538f48c8e8d0644e5993dd4f65964e3f8
sechiyo97 <sechiyo97@daum.net>
2022-04-28 03:57:34 +0900

    add solution for 3

--- src/main/kotlin/solution/Sol_3_longest_substring_without_repeating_characters.kt
+++ src/main/kotlin/solution/Sol_3_longest_substring_without_repeating_characters.kt
@@ -6,9 +6,34 @@ class Sol_3_longest_substring_without_repeating_characters : Solution.General<St
         "bbbbb" to 1,
         "pwwkew" to 3
     )
+    override val customTestCases: Map<String, Int> = mapOf(
+        " " to 1,
+        "dvdf" to 3
+    )
 
     override fun algorithm(input: String): Int {
-        return 0
+        val s = input
+
+        val idxArr = IntArray(256) { -1 }
+
+        var left=0
+        var right=0
+        var len=0
+        var maxLen=0
+        while(right < s.length){
+            val arrIdx = s[right].toInt()%256
+            val isRightInWindow =  idxArr[arrIdx] >= left
+
+            if (isRightInWindow) {
+                left = idxArr[arrIdx] + 1
+            }
+            idxArr[arrIdx] = right
+            len = right-left+1
+            maxLen = maxLen.coerceAtLeast(len)
+
+            right++
+        }
+        return maxLen.coerceAtLeast(len)
     }
 
     override fun inputStringToInputType(input: String): String {
