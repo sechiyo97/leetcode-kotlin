e44eb29cf1b09922b3211048f9bf318758b23ba3
sechiyo97 <sechiyo97@daum.net>
2022-04-27 12:26:36 +0900

    create class for 167

--- src/main/kotlin/solution/Solution167_TwoSumIiInputArrayIsSorted.kt
+++ src/main/kotlin/solution/Solution167_TwoSumIiInputArrayIsSorted.kt
@@ -2,32 +2,17 @@ package solution
 
 import common.toIntArray
 
-class Solution35_SearchInsertPosition: Solution.General<Solution35_SearchInsertPosition.Params, Int>(){
-    class Params(val nums: IntArray, val target: Int)
+class Solution167_TwoSumIiInputArrayIsSorted: Solution.General<Solution167_TwoSumIiInputArrayIsSorted.Params, IntArray>(){
+    class Params(val numbers: IntArray, val target: Int)
 
-    override val givenTestCases: Map<String, Int> = mapOf(
-        "[1,3,5,6] 5" to 2,
-        "[1,3,5,6] 2" to 1,
-        "[1,3,5,6] 7" to 4
+    override val givenTestCases: Map<String, IntArray> = mapOf(
+        "[2,7,11,15] 9" to intArrayOf(1,2),
+        "[2,3,4] 6" to intArrayOf(1,3),
+        "[-1,0] -1" to intArrayOf(1,2)
     )
 
-    override fun algorithm(input: Params): Int {
-        val nums = input.nums
-        val target = input.target
-
-        var min = 0
-        var max = nums.lastIndex
-        var mid = (min + max)/2
-        while (max >= min) {
-            when {
-                target > nums[mid] -> min = mid + 1
-                target < nums[mid] -> max = mid - 1
-                else -> return mid
-            }
-            mid = (min/2.0 + max/2.0).toInt()
-        }
-
-        return max+1
+    override fun algorithm(input: Params): IntArray {
+        return input.numbers
     }
 
     override fun inputStringToInputType(input: String): Params {
