2defc2cbbfa15eac81b2a0f9b1a595c3bca5d443
sechiyo97 <sechiyo97@daum.net>
2022-04-29 00:35:25 +0900

    add solution 567
    (cherry picked from commit 0c81433422c092591b8a9fbda8b0713c9f2de0d6)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
+import solution.Sol_512_permutation_in_string
 import solution.sample.SampleSolution_General
 
-val solution = SampleSolution_General() // edit here
+val solution = Sol_512_permutation_in_string() // edit here
 
 fun main() {
     solution.testWithGivenCases() // for given tests
     solution.testWithCustomCases() // for custom tests
-    solution.runWithInputCase() // for user input case
+//    solution.runWithInputCase() // for user input case
 }
