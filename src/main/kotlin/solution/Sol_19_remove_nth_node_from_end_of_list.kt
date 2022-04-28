2fb55d9172e73374d5c6cf17a2370c8fc14c4169
sechiyo97 <sechiyo97@daum.net>
2022-04-29 04:00:23 +0900

    change int array -> listnode method

--- src/main/kotlin/solution/Sol_19_remove_nth_node_from_end_of_list.kt
+++ src/main/kotlin/solution/Sol_19_remove_nth_node_from_end_of_list.kt
@@ -2,16 +2,15 @@ package solution
 
 import Solution
 import common.model.ListNode
-import common.model.toHeadListNode
 import common.toIntArray
 
 class Sol_19_remove_nth_node_from_end_of_list: Solution.General<Sol_19_remove_nth_node_from_end_of_list.Params, ListNode?>(){
     data class Params(val head: ListNode?, val n: Int)
 
     override val givenTestCases: Map<String, ListNode?> = mapOf(
-        "[1,2,3,4,5] 2" to intArrayOf(1,2,3,5).toHeadListNode(),
-        "[1] 1" to intArrayOf().toHeadListNode(),
-        "[1,2] 1" to intArrayOf(1).toHeadListNode()
+        "[1,2,3,4,5] 2" to ListNode.fromIntArray(intArrayOf(1,2,3,5)),
+        "[1] 1" to ListNode.fromIntArray(intArrayOf()),
+        "[1,2] 1" to ListNode.fromIntArray(intArrayOf(1))
     )
 
     override fun algorithm(input: Params): ListNode? {
@@ -34,7 +33,7 @@ class Sol_19_remove_nth_node_from_end_of_list: Solution.General<Sol_19_remove_nt
 
     override fun inputStringToInputType(input: String): Params {
         val split = input.split(" ")
-        val head = split[0].toIntArray().toHeadListNode()
+        val head = ListNode.fromIntArray(split[0].toIntArray())
         val n = split[1].toInt()
         return Params(head, n)
     }
