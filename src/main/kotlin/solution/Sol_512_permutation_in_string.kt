855a85f5aaa9203cb629e41f8ec74b56c063d1d0
sechiyo97 <sechiyo97@daum.net>
2022-04-29 00:36:01 +0900

    create problem 567 class

---
+++ src/main/kotlin/solution/Sol_512_permutation_in_string.kt
@@ -0,0 +1,22 @@
+package solution
+
+import Solution
+
+class Sol_512_permutation_in_string: Solution.General<Sol_512_permutation_in_string.Params, Boolean>() {
+    data class Params(val s1: String, val s2: String)
+    override val givenTestCases: Map<String, Boolean> = mapOf(
+        "ab eidbaooo" to true,
+        "ab eidboaoo" to false
+    )
+
+    override fun algorithm(params: Params): Boolean {
+        return false
+    }
+
+    override fun inputStringToInputType(input: String): Params {
+        val split = input.split(" ")
+        return Params(split[0], split[1])
+    }
+
+}
+
