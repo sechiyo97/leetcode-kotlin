e60a65844302e2b5b80853adddb3ad337ded40b7
sechiyo97 <sechiyo97@daum.net>
2022-04-29 01:26:22 +0900

    add sol 733

--- src/main/kotlin/solution/Sol_733_flood_fill.kt
+++ src/main/kotlin/solution/Sol_733_flood_fill.kt
@@ -9,9 +9,41 @@ class Sol_733_flood_fill: Solution.General<Sol_733_flood_fill.Params, Array<IntA
         "[[1,1,1],[1,1,0],[1,0,1]] 1 1 2" to arrayOf(intArrayOf(2,2,2), intArrayOf(2,2,0), intArrayOf(2,0,1)),
         "[[0,0,0],[0,0,0]] 0 0 2" to arrayOf(intArrayOf(2,2,2), intArrayOf(2,2,2))
     )
+    override val customTestCases: Map<String, Array<IntArray>> = mapOf(
+        "[[0,0,0],[0,1,0]] 1 1 2" to arrayOf(intArrayOf(0,0,0), intArrayOf(0,2,0)),
+        "[[0,0,0],[0,1,1]] 1 1 1" to arrayOf(intArrayOf(0,0,0), intArrayOf(0,1,1))
+    )
 
     override fun algorithm(input: Params): Array<IntArray> {
-        return arrayOf(intArrayOf(2,2,2), intArrayOf(2,2,2))
+        val image = input.image
+        val sr = input.sr
+        val sc = input.sc
+        val newColor = input.newColor
+
+        val orgColor = image[sr][sc]
+        image[sr][sc] = newColor
+        image.floodFill4Direction(sr, sc, orgColor, newColor)
+        return image
+    }
+
+    private fun Array<IntArray>.floodFill4Direction(sr: Int, sc: Int, orgColor: Int, newColor: Int) {
+        if (this.isEmpty()) return
+        if (sr < this.lastIndex && this[sr+1][sc] == orgColor && this[sr+1][sc] != newColor) {
+            this[sr+1][sc] = newColor
+            this.floodFill4Direction(sr+1, sc, orgColor, newColor)
+        }
+        if (sr > 0 && this[sr-1][sc] == orgColor && this[sr-1][sc] != newColor) {
+            this[sr-1][sc] = newColor
+            this.floodFill4Direction(sr-1, sc, orgColor, newColor)
+        }
+        if (sc < this[0].lastIndex && this[sr][sc+1] == orgColor && this[sr][sc+1] != newColor) {
+            this[sr][sc+1] = newColor
+            this.floodFill4Direction(sr, sc+1, orgColor, newColor)
+        }
+        if (sc > 0 && this[sr][sc-1] == orgColor && this[sr][sc-1] != newColor) {
+            this[sr][sc-1] = newColor
+            this.floodFill4Direction(sr, sc-1, orgColor, newColor)
+        }
     }
 
     override fun inputStringToInputType(input: String): Params {
