e909e9adf33afee41374eee374ea4c320a097ef1
sechiyo97 <sechiyo97@daum.net>
2022-04-30 01:24:43 +0900

    add sol 21

--- src/main/kotlin/solution/Sol_21_merge_two_sorted_lists.kt
+++ src/main/kotlin/solution/Sol_21_merge_two_sorted_lists.kt
@@ -12,9 +12,33 @@ class Sol_21_merge_two_sorted_lists: Solution.General<Sol_21_merge_two_sorted_li
         "[] []" to null,
         "[] [0]" to ListNode.fromIntArray(intArrayOf(0))
     )
+    override val customTestCases: Map<String, ListNode?> = mapOf(
+        "[1] []" to ListNode.fromIntArray(intArrayOf(1))
+    )
 
     override fun algorithm(input: Params): ListNode? {
-        return input.list1
+        val list1 = input.list1
+        val list2 = input.list2
+
+        var node1 = list1
+        var node2 = list2
+
+        val dummyResult = ListNode(-1)
+        var nodeResult: ListNode? = dummyResult
+
+        while (node1 != null && node2 != null) {
+            if(node1.`val` > node2.`val`) {
+                nodeResult?.next = node2
+                node2 = node2.next
+            } else {
+                nodeResult?.next = node1
+                node1 = node1.next
+            }
+            nodeResult = nodeResult?.next
+        }
+        nodeResult?.next = node1 ?: node2
+
+        return dummyResult.next
     }
 
     override fun inputStringToInputType(input: String): Params {
