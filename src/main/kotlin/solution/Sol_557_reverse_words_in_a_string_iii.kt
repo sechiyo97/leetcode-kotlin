d0363c644bf3a5013ef673c75d5d27dea29d405d
sechiyo97 <sechiyo97@daum.net>
2022-04-29 22:52:50 +0900

    rename problem 557

--- src/main/kotlin/solution/Sol_557_reverse_words_in_a_string_iii.kt
+++ src/main/kotlin/solution/Sol_557_reverse_words_in_a_string_iii.kt
@@ -2,7 +2,7 @@ package solution
 
 import Solution
 
-class Sol_557_reverse_words_in_a_string: Solution.General<String, String>() {
+class Sol_557_reverse_words_in_a_string_iii: Solution.General<String, String>() {
     override val givenTestCases: Map<String, String> = mapOf(
         "Let's take LeetCode contest" to "s'teL ekat edoCteeL tsetnoc",
         "God Ding" to "doG gniD"
