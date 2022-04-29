dcb4d7b5f709c6b35ff84b39878210160ed26444
sechiyo97 <sechiyo97@daum.net>
2022-04-29 20:37:26 +0900

    fix typo

--- src/main/kotlin/solution/sample/Sol_542_01_matrix.kt
+++ src/main/kotlin/solution/sample/Sol_542_01_matrix.kt
@@ -5,13 +5,13 @@ import common.to2DIntArray
 class Sol_542_01_matrix: Solution.General<Array<IntArray>, Array<IntArray>>() {
     override val givenTestCases: Map<String, Array<IntArray>> = mapOf(
         "[[0,0,0],[0,1,0],[0,0,0]]" to "[[0,0,0],[0,1,0],[1,2,1]]".to2DIntArray(),
-        "[[0,0,0],[0.1,0],[1,1,1]]" to "[[0,0,0],[0,1,0],[0,0,0]]".to2DIntArray()
+        "[[0,0,0],[0,1,0],[1,1,1]]" to "[[0,0,0],[0,1,0],[0,0,0]]".to2DIntArray()
     )
 
     override fun algorithm(input: Array<IntArray>): Array<IntArray> {
         return input
     }
-    
+
     override fun inputStringToInputType(input: String): Array<IntArray> {
         return input.to2DIntArray()
     }
