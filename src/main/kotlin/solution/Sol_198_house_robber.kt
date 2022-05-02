336d428eee09682c3ea2b1f706202034b874e311
sechiyo97 <sechiyo97@daum.net>
2022-05-02 16:25:35 +0900

    add sol 198

--- src/main/kotlin/solution/Sol_198_house_robber.kt
+++ src/main/kotlin/solution/Sol_198_house_robber.kt
@@ -5,7 +5,18 @@ import common.toIntArray
 // https://leetcode.com/problems/house-robber/
 class Sol_198_house_robber: Solution.General<IntArray, Int>() {
     fun rob(nums: IntArray): Int {
-        return 0
+        if (nums.size == 1) return nums[0]
+        var result1 = nums[0]
+        var result2 = if (nums[0] > nums[1]) nums[0] else nums[1]
+        var temp: Int
+        for (i in 2 until nums.size) {
+            val compare1 = result2
+            val compare2 = result1 + nums[i]
+            temp = result2
+            result2 = if (compare1 > compare2) compare1 else compare2
+            result1 = temp
+        }
+        return result2
     }
 
     override val givenTestCases: Map<String, Int> = mapOf(
