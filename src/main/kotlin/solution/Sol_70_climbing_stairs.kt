5ff544ab56643b01d280fa4ce31676741b3d64d7
sechiyo97 <sechiyo97@daum.net>
2022-05-02 16:09:58 +0900

    update sol 70

--- src/main/kotlin/solution/Sol_70_climbing_stairs.kt
+++ src/main/kotlin/solution/Sol_70_climbing_stairs.kt
@@ -4,20 +4,24 @@ import Solution
 
 // https://leetcode.com/problems/climbing-stairs/
 class Sol_70_climbing_stairs: Solution.General<Int, Int>() {
-    val results = IntArray(46)
-    init {
-        results[0] = 1
-        results[1] = 1
-    }
+
     fun climbStairs(n: Int): Int {
-        for (i in 2 until n+1) results[i] = results[i-1] + results[i-2]
-        return results[n]
+        if (n==1) return 1
+        var result1 = 1
+        var result2 = 2
+        for (i in 2 until n) {
+            val temp = result2
+            result2 += result1
+            result1 = temp
+        }
+        return result2
     }
 
     override val givenTestCases: Map<String, Int> = mapOf(
         "2" to 2,
         "3" to 3,
-        "44" to 1134903170
+        "44" to 1134903170,
+        "1" to 1
     )
 
     override fun inputStringToInputType(input: String): Int = input.toInt()
