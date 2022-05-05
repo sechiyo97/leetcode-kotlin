1abcb7117ebc5422ba656c555552a6f845d91b91
sechiyo97 <sechiyo97@daum.net>
2022-05-05 23:03:16 +0900

    add solution with one queue

--- src/main/kotlin/solution/Sol_225_implement_stack_using_queues.kt
+++ src/main/kotlin/solution/Sol_225_implement_stack_using_queues.kt
@@ -13,37 +13,62 @@ fun main(){
 }
 
 class Sol_225_implement_stack_using_queues {
+    /**
+     * Solution 2
+     * one queue
+     */
     class MyStack() {
-        val queue0 = LinkedList<Int>()
-        val queue1 = LinkedList<Int>()
+        val queue = LinkedList<Int>()
 
         fun push(x: Int) {
-            if (x%2==0) {
-                queue0.offer(x)
-                while (queue1.isEmpty().not()) {
-                    queue0.offer(queue1.poll())
-                }
-            } else {
-                queue1.offer(x)
-                while (queue0.isEmpty().not()) {
-                    queue1.offer(queue0.poll())
-                }
+            queue.offer(x)
+            for (i in 0 until queue.size-1) {
+                val value = queue.poll()
+                queue.offer(value)
             }
         }
 
-        fun pop(): Int {
-            return if (queue0.isEmpty()) queue1.poll()
-            else queue0.poll()
-        }
-
-        fun top(): Int {
-            return if (queue0.isEmpty()) queue1.peek()
-            else queue0.peek()
-        }
-
-        fun empty(): Boolean {
-            return queue0.isEmpty() && queue1.isEmpty()
-        }
-
+        fun pop(): Int = queue.poll()
+        fun top(): Int = queue.peek()
+        fun empty(): Boolean = queue.isEmpty()
     }
+
+
+
+    /**
+     * Solution 1
+     * two queues
+     */
+//    class MyStack() {
+//        val queue0 = LinkedList<Int>()
+//        val queue1 = LinkedList<Int>()
+//
+//        fun push(x: Int) {
+//            if (x%2==0) {
+//                queue0.offer(x)
+//                while (queue1.isEmpty().not()) {
+//                    queue0.offer(queue1.poll())
+//                }
+//            } else {
+//                queue1.offer(x)
+//                while (queue0.isEmpty().not()) {
+//                    queue1.offer(queue0.poll())
+//                }
+//            }
+//        }
+//
+//        fun pop(): Int {
+//            return if (queue0.isEmpty()) queue1.poll()
+//            else queue0.poll()
+//        }
+//
+//        fun top(): Int {
+//            return if (queue0.isEmpty()) queue1.peek()
+//            else queue0.peek()
+//        }
+//
+//        fun empty(): Boolean {
+//            return queue0.isEmpty() && queue1.isEmpty()
+//        }
+//    }
 }
