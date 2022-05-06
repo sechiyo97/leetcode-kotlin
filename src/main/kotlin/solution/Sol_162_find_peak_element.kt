6523464a22533f76654e8f7873b8974f526ccbfc
sechiyo97 <sechiyo97@daum.net>
2022-05-06 21:24:15 +0900

    create sol 162

--- src/main/kotlin/solution/Sol_162_find_peak_element.kt
+++ src/main/kotlin/solution/Sol_162_find_peak_element.kt
@@ -1,4 +1,29 @@
 package solution
 
-class Sol_162_find_peak_element {
+import Solution
+import common.toIntArray
+import kotlin.math.log10
+
+class Sol_162_find_peak_element: Solution.General<IntArray, Int>() {
+    fun findPeakElement(nums: IntArray): Int {
+        return 0
+    }
+
+    override val givenTestCases: Map<String, Int> = mapOf(
+        "[1,2,3,1]" to 2*1,
+        "[1,2,1,3,5,6,4]" to 2*1000+5*1
+    )
+
+    override fun inputStringToInputType(input: String): IntArray = input.toIntArray()
+    override fun algorithm(input: IntArray): Int = findPeakElement(input)
+    override fun checkEquals(result: Int, expected: Int?): Boolean {
+        if (expected == null) return false
+        var value = expected
+        var availableResults = IntArray(log10(expected.toDouble()).toInt()/3 + 1) {
+            val left = value%1000
+            value/=1000
+            left
+        }
+        return availableResults.contains(result)
+    }
 }
