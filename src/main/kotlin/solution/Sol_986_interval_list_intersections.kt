216cc9a785d79edbd1f1ef66fed325db7d9c5502
sechiyo97 <sechiyo97@daum.net>
2022-05-07 01:06:47 +0900

    add sol 986

--- src/main/kotlin/solution/Sol_986_interval_list_intersections.kt
+++ src/main/kotlin/solution/Sol_986_interval_list_intersections.kt
@@ -6,7 +6,26 @@ import common.to2DIntArray
 // https://leetcode.com/problems/interval-list-intersections/
 class Sol_986_interval_list_intersections: Solution.General<Sol_986_interval_list_intersections.Params, Array<IntArray>>() {
     fun intervalIntersection(firstList: Array<IntArray>, secondList: Array<IntArray>): Array<IntArray> {
-        return firstList
+        val result = mutableListOf<IntArray>()
+        var i = 0
+        var j = 0
+        while(i < firstList.size && j < secondList.size) {
+            // add intersection
+            val startTime = firstList[i][0].coerceAtLeast(secondList[j][0])
+            val endTime = firstList[i][1].coerceAtMost(secondList[j][1])
+            if (endTime >= startTime) result.add(intArrayOf(startTime, endTime))
+
+            // next
+            val nextStartTime1 = if (i < firstList.size-1) firstList[i+1][0] else null
+            val nextStartTime2 = if (j < secondList.size-1) secondList[j+1][0] else null
+            when {
+                nextStartTime1 == null -> j++
+                nextStartTime2 == null -> i++
+                nextStartTime1 > nextStartTime2 -> j++
+                else -> i++
+            }
+        }
+        return result.toTypedArray()
     }
 
     override val givenTestCases: Map<String, Array<IntArray>> = mapOf(
