8ceaa293a52af0af6d69a5ecabed10db18f32770
sechiyo97 <sechiyo97@daum.net>
2022-05-25 23:55:50 +0900

    update sol 354 to match leetcode kotlin version

--- src/main/kotlin/solution/Sol_354_russian_doll_envelopes.kt
+++ src/main/kotlin/solution/Sol_354_russian_doll_envelopes.kt
@@ -3,9 +3,9 @@ package solution
 import Solution
 import common.to2DIntArray
 
-class Sol_354_russian_doll_envelopes: Solution.General<Array<IntArray>, Int>() {
+class Sol_354_russian_doll_envelopes : Solution.General<Array<IntArray>, Int>() {
     fun maxEnvelopes(envelopes: Array<IntArray>): Int {
-        envelopes.sortWith { a, b -> if (a[0] == b[0]) b[1] - a[1] else a[0] - b[0] }
+        envelopes.sortWith(Comparator { a, b -> if (a[0] == b[0]) b[1] - a[1] else a[0] - b[0] })
         // largestHeights[i]: the largest-height doll of a valid list that consists of i dolls
         val largestHeights = mutableListOf<Int>()
 
@@ -13,7 +13,7 @@ class Sol_354_russian_doll_envelopes: Solution.General<Array<IntArray>, Int>() {
             var left = 0
             var right = largestHeights.size
             while (left < right) {
-                val mid = (right+left) shr 1
+                val mid = (right + left) shr 1
                 if (largestHeights[mid] >= it[1]) right = mid
                 else left = mid + 1
             }
