75af7ea8ece1db0a04d0c6ada6c61f4c0a9af233
sechiyo97 <sechiyo97@gmail.com>
2025-03-30 02:41:13 +0900

    add sol 31

---
+++ src/main/kotlin/solution/Sol_31_next_permutation.kt
@@ -0,0 +1,41 @@
+package solution
+
+import Solution
+import common.swap
+import common.toIntArray
+
+/**
+ * In actual problem, default return value is Unit (in-place replacement).
+ * But for code consistency with other problems,
+ * this solution uses the input IntArray as return value.
+ */
+// https://leetcode.com/problems/next-permutation/
+class Sol_31_next_permutation: Solution.General<IntArray, IntArray>() {
+    fun nextPermutation(nums: IntArray): IntArray {
+        val downsideIndexFromEnd = nums.indices.reversed().first { it <= 0 || nums[it] > nums[it-1] }
+        if (downsideIndexFromEnd <= 0) {
+            nums.sort()
+        } else {
+            val index1 = downsideIndexFromEnd - 1
+            val index2 = nums.indices.reversed().first { nums[it] > nums[index1] }
+            nums.swap(index1, index2)
+            nums.sort(index1 + 1, nums.size)
+        }
+        return nums
+    }
+
+    override val givenTestCases: Map<String, IntArray> = mapOf(
+        "[1,2,3]" to intArrayOf(1,3,2),
+        "[3,2,1]" to intArrayOf(1,2,3),
+        "[1,1,5]" to intArrayOf(1,5,1),
+    )
+    override val customTestCases: Map<String, IntArray> = mapOf(
+        "[2,3,1]" to intArrayOf(3,1,2),
+        "[1,3,2]" to intArrayOf(2,1,3),
+    )
+
+    override fun algorithm(input: IntArray): IntArray = nextPermutation(input)
+    override fun inputStringToInputType(input: String): IntArray {
+        return input.toIntArray()
+    }
+}
